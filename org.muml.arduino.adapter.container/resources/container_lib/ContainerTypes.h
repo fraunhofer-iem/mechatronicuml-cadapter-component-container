099ff7dfb809d05c1ab641e161dbd61b84f4777f
Code-Schwabe <st170535@stud.uni-stuttgart.de>
2022-03-22 22:29:00 +0100

    Library Files

---
+++ org.muml.arduino.adapter.container/resources/container_lib/ContainerTypes.h
@@ -0,0 +1,32 @@
+#ifndef CONTAINER_TYPES_
+#define CONTAINER_TYPES_
+
+//available per ECU across container
+//copied from orglmuml.c.adapter.container/resources/container_lib due to necessary adjustments
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//Global for all Container (e.g. Lib_Container)
+#include "../lib/port.h"
+#include "MessageBuffer.h"
+
+
+typedef enum {
+	PORT_HANDLE_TYPE_MQTT, PORT_HANDLE_TYPE_I2C
+} HandleType;
+
+typedef struct PortHandle {
+	HandleType type;
+	Port* port;
+	void *concreteHandle;
+} PortHandle;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONTAINER_TYPES_ */
